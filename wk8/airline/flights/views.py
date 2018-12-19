438a63312fef2b11269e0aa6b7e7668a83a8f4b9
Tim Corley <tcorley215@gmail.com>
2018-12-18 21:32:10 -0500

    adding first batch wk8 files

---
+++ wk8/airline/flights/views.py
@@ -0,0 +1,10 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+from .models import Flight
+
+# Create your views here.
+def index(request):
+    context = {
+        "flights": Flight.objects.all()
+    }
+    return render(request, "flights/index.html", context)
